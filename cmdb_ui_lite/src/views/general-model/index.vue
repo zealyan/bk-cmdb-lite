b5ff289b6ca13b3d2d26d6288a4840e411d29ca3
zealyan <zealyan@users.noreply.github.com>
2026-06-01 10:13:29 +0000

    feat: 拉取代码
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- cmdb_ui_lite/src/views/general-model/index.vue
+++ cmdb_ui_lite/src/views/general-model/index.vue
@@ -298,7 +298,8 @@ export default {
           count: 0,
           current: 1,
           limit: 10,
-          show: true
+          show: true,
+          'limit-list': [10, 20, 50, 100, 500]
         },
         loading: false,
         displayFields: []
