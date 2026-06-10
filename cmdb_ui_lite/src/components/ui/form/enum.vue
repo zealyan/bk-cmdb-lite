e367896df6948d4bcfc2f90dc2bfef932b472bf9
zealyan <zealyan@users.noreply.github.com>
2026-06-10 05:53:53 +0000

    feat: 拉取代码
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- cmdb_ui_lite/src/components/ui/form/enum.vue
+++ cmdb_ui_lite/src/components/ui/form/enum.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="g-expand">
-    <bk-select class="form-enum-selector"
+  <bk-select class="form-enum-selector"
       v-model="selected"
       :clearable="allowClear"
       :searchable="true"
@@ -21,7 +20,6 @@
         :name="option.name">
       </bk-option>
     </bk-select>
-  </div>
 </template>
 
 <script>
